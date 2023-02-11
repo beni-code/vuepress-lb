214992837c2f0a1bbf70d65dfc25f04af236691c
liben <714376485@qq.com>
2023-02-11 20:54:36 +0800

    1

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,8 +7,7 @@ export default defineUserConfig({
   locales: {
     "/": {
       lang: "zh-CN",
-      title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
+      title: "Notes"
     },
   },
 
